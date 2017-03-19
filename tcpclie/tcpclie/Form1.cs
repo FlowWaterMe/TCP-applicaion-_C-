72b61c40674a374398bb5291529af1893ee687bb
young <heyong.liu@intelligentgroup.cn>
2017-03-19 17:08:34 +0800

    tcp_client& server
    this is client and server user c#

---
+++ tcpclie/tcpclie/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net.Sockets;
+using System.Threading;
+using System.Net;
+using System.Collections;
+namespace tcpclie
+{
+    public partial class Form1 : Form
+    {
+        private TcpClient client;
+        private Thread client_th;
+        public Form1()
+        {
+            InitializeComponent();
+            Control.CheckForIllegalCrossThreadCalls = false;
+         
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textPort.Text != "" && textIP.Text != "")
+            {
+                string mystr = textIP.Text;
+                string ip = mystr;
+                //1.intA =int.Parse(str);  string to int
+                //2.int.TryParse(str, out intA);
+                //3.intA = Convert.ToInt32(str);
+                int port = int.Parse(textPort.Text);
+                this.connect_s(ip, port);
+            }
+             else
+            {
+                MessageBox.Show("please input ip and port,then connect net");
+            }
+
+        }
+        public void connect_s(string ip,int port)
+        {
+            try
+            {
+                client = new TcpClient(ip, port);
+                ThreadStart threadStart = new ThreadStart(AcceptMsg);
+                client_th = new Thread(threadStart);
+                client_th.Start();
+                textBox2.Text = "连接成功";
+
+            }
+            catch (System.Exception e)
+            {
+                textBox2.Text = e.ToString(); 
+
+            }
+        }
+        private void AcceptMsg()
+        {
+            NetworkStream ns = client.GetStream();
+
+            //StreamReader sr = new StreamReader(ns);//流读写器
+            //字组处理
+            while (true)
+            {
+                try
+                {
+                    byte[] bytes = new byte[1024];
+                    int bytesread = ns.Read(bytes, 0, bytes.Length);
+                    string msg = Encoding.Default.GetString(bytes, 0, bytesread);
+                    //显示
+                    //MessageBox.Show(msg);
+                    textBox2.Text = msg;
+                    ns.Flush();
+                    //ns.Close();
+                }
+                catch (System.Exception e)
+                {
+                    textBox2.Text = e.ToString(); 
+                    MessageBox.Show("与服务器断开连接了");
+                    break;
+                }
+            }
+        }
+ 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (client == null)
+            {
+                MessageBox.Show("client is disconnect,should connect and input message again");
+                return;
+            }
+            NetworkStream sendStream = client.GetStream();
+            String msg = textBox1.Text;
+            Byte[] sendBytes = Encoding.Default.GetBytes(msg);
+            sendStream.Write(sendBytes, 0, sendBytes.Length);
+            sendStream.Flush();
+            //sendStream.Close();
+            textBox1.Text = "Msg Sent!";
+        
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ tcpip/tcpip/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net.Sockets;
+using System.Threading;
+using System.Net;
+using System.Collections;
+namespace tcpip
+{
+    public partial class form1 : Form
+    {
+        private ThreadStart start;
+        private Thread listenThread, client_th;
+        static private bool m_bListening = false;
+        static private System.Net.IPAddress MyIP = System.Net.IPAddress.Parse("127.0.0.2");//127.0.0.2
+        static private TcpListener listener = new TcpListener(MyIP, 5567);
+
+        
+        private String msg;
+        ArrayList clientArray = new ArrayList();
+
+        public form1()
+        {
+            InitializeComponent();
+            Control.CheckForIllegalCrossThreadCalls = false;
+            start = new ThreadStart(startListen);//创建Thread类的对象，生成一个子线程
+            listenThread = new Thread(start); 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (m_bListening)
+            {
+                m_bListening = false;
+                label2.Text = "Server Idle...";
+            }
+            else
+            {
+                m_bListening = true;
+                if (TextIp.Text!="" && textPort.Text != "" )
+                {
+                    string strip = TextIp.Text;
+                    int port = int.Parse(textPort.Text);
+                    System.Net.IPAddress MyIP = System.Net.IPAddress.Parse(strip);//127.0.0.2
+                    TcpListener listener = new TcpListener(MyIP, port);
+                    start = new ThreadStart(startListen);//创建Thread类的对象，生成一个子线程
+                    listenThread = new Thread(start); 
+                }
+                listenThread.Start();
+                label2.Text = "Server Listening...";
+            }
+        }
+        private void showmore()
+        {
+            textBox1.Text = msg;
+        }
+
+        private void startListen()
+        {
+            listener.Start();
+            while (m_bListening)
+            {
+                try
+                {
+                    TcpClient client = listener.AcceptTcpClient();
+                    clientArray.Add(client);
+                    ParameterizedThreadStart threadStart = new ParameterizedThreadStart(AcceptMsg);
+                    client_th = new Thread(threadStart);
+                    client_th.Start(client);
+
+                }
+                catch (Exception re)
+                {
+                    MessageBox.Show(re.Message);
+                }
+            }
+            listener.Stop();
+        }
+
+        private void AcceptMsg(object arg)
+        {
+            TcpClient client = (TcpClient)arg;
+            NetworkStream ns = client.GetStream();
+            while (true)
+            {
+                try
+                {
+                    byte[] bytes = new byte[1024];
+                    int bytesread = ns.Read(bytes, 0, bytes.Length);
+                    msg = Encoding.Default.GetString(bytes, 0, bytesread);
+                    //显示
+                    //MessageBox.Show(msg);
+                    showmore();
+                    ns.Flush();
+                    //ns.Close();
+                }
+                catch
+                {
+                    MessageBox.Show("与客户端断开连接");
+                    break;
+                }
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text!="")
+            {
+                object a = clientArray[0];
+                TcpClient client = (TcpClient)clientArray[0];
+                if (client == null)
+                    return;
+                NetworkStream sendStream = client.GetStream();
+                String msg = textBox2.Text;
+                Byte[] sendBytes = Encoding.Default.GetBytes(msg);
+                sendStream.Write(sendBytes, 0, sendBytes.Length);
+                sendStream.Flush();
+            }
+            else{
+                MessageBox.Show("please input message to box");
+            }
+            //sendStream.Close();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
